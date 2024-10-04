33ae7769647edab4a9150409c869914832be55eb
Samer Afach <info@afach.de>
2024-10-04 13:28:34 +0400

    Remove casting to TryFrom

--- src/hasher/options/traits.rs
+++ src/hasher/options/traits.rs
@@ -39,7 +39,7 @@ pub trait HashingOptions: TryFrom<BTreeMap<String, String>, Error = anyhow::Erro
             }
         }
 
-        let result = <Self as TryFrom<_>>::try_from(options)?;
+        let result = Self::try_from(options)?;
 
         Ok(result)
     }
